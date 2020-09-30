bb9e94a944c2d49c2b302578e61e34a5cf9efb3b
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-09-29 23:22:19 -0700

    Finished Articles Page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,63 +1,72 @@
-import React from "react"
-import Container from "react-bootstrap/Container"
-import Head from "../components/Head"
-import Layout from "../components/Layout"
-import CardDeck from "react-bootstrap/CardDeck"
-import Card from "react-bootstrap/Card"
-import "../../scss/articles.scss"
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Layout from '../components/Layout'
+import Head from '../components/Head'
+import '../../scss/articles.scss'
 
 const allArticles = [
   {
     id: 0,
-    text: "Omy de Oro signs distribution deal with Caroline and releases new single “Cash” - AfterBuzz TV",
+    text:
+      'Omy de Oro signs distribution deal with Caroline and releases new single “Cash” - AfterBuzz TV',
     title: "Meet Puerto Rico's King of Punchline Omy de Oro",
-    url: "https://www.afterbuzztv.com/meet-puerto-ricos-king-of-punchline-omy-de-oro/"
+    url:
+      'https://www.afterbuzztv.com/meet-puerto-ricos-king-of-punchline-omy-de-oro/',
   },
   {
     id: 1,
-    text: "Debido a la crisis de salud y estos tiempos de cuarentena, nos enfrentamos a una nueva realidad, pero los organizadores de Hola México Film Festival no han rendido - AfterBuzz TV",
-    title: "Hola Mexico Film Festival Presenta Su Primer Cine Virtual!",
-    url: "https://www.afterbuzztv.com/hola-mexico-film-festival-presenta-su-primer-cine-virtual/"
+    text:
+      'Debido a la crisis de salud y estos tiempos de cuarentena, nos enfrentamos a una nueva realidad, pero los organizadores de Hola México Film Festival no han rendido - AfterBuzz TV',
+    title: 'Hola Mexico Film Festival Presenta Su Primer Cine Virtual!',
+    url:
+      'https://www.afterbuzztv.com/hola-mexico-film-festival-presenta-su-primer-cine-virtual/',
   },
   {
     id: 2,
-    text: "Founded in 2008, the Hola Mexico Film Festival strives to highlight the best and brightest talents in Mexican cinematography. Despite Covid, fest will continue virtually as AfterBuzz Latino reports - AfterBuzz TV",
-    title: "Hola Mexico Film Festival Goes Virtual!",
-    url: "https://www.afterbuzztv.com/hola-mexico-film-festival-goes-virtual/"
+    text:
+      'Founded in 2008, the Hola Mexico Film Festival strives to highlight the best and brightest talents in Mexican cinematography. Despite Covid, fest will continue virtually as AfterBuzz Latino reports - AfterBuzz TV',
+    title: 'Hola Mexico Film Festival Goes Virtual!',
+    url: 'https://www.afterbuzztv.com/hola-mexico-film-festival-goes-virtual/',
   },
   {
     id: 3,
-    text: "Frankie Rodriguez Breaks Barriers as One of First Gay Couples On High School Musical - AfterBuzz TV",
-    title: "Frankie Rodriguez Breaks Barriers as One of First Gay Couples On High School MusicalMeet Puerto Rico's King of Punchline Omy de Oro",
-    url: "https://www.afterbuzztv.com/frankie-rodriguez-breaks-barriers-as-one-of-first-gay-couples-on-high-school-musical/"
+    text:
+      'Frankie Rodriguez Breaks Barriers as One of First Gay Couples On High School Musical - AfterBuzz TV',
+    title:
+      "Frankie Rodriguez Breaks Barriers as One of First Gay Couples On High School MusicalMeet Puerto Rico's King of Punchline Omy de Oro",
+    url:
+      'https://www.afterbuzztv.com/frankie-rodriguez-breaks-barriers-as-one-of-first-gay-couples-on-high-school-musical/',
   },
 ]
 
-const Articles = () => {
-  const articleCards = allArticles.map(article => {
-    return (
-      <Card className="card-element col-md-12" key={article.id}>
-        <Card.Body>
-          <Card.Title>{article.title}</Card.Title>
-          <Card.Text>{article.text}</Card.Text>
-          <a className="read-now-btn btn" href={article.url}>Read Now</a>
-        </Card.Body>
-      </Card>
-    )
-  })
-  return (
-    <Layout>
-      <>
-        <Head title="Articles" />
-        <Container>
-          <h1>Articles</h1>
-          <CardDeck>
-            {articleCards}
-          </CardDeck>
-        </Container>
-      </>
-    </Layout>
-  )
-}
+const articleCards = allArticles.map(article => (
+  <Col className="card-column col-12 col-md-6 col-lg-3 mb-3" key={article.id}>
+    <Card className="card-element text-center h-100">
+      <Card.Body>
+        <Card.Title>{article.title}</Card.Title>
+        <Card.Text>{article.text}</Card.Text>
+        <a className="read-now-btn btn" href={article.url}>
+          Read Now
+        </a>
+      </Card.Body>
+    </Card>
+  </Col>
+))
+
+const Articles = () => (
+  <Layout>
+    <>
+      <Head title="Articles" />
+      <Container>
+        <h1>Articles</h1>
+        <Row className="justify-content-center">{articleCards}</Row>
+      </Container>
+    </>
+  </Layout>
+)
 
 export default Articles

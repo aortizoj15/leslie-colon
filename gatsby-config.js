2ba6e7d18016ad64bb793aab67619d9cecfa26ec
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-10-28 22:24:36 -0700

    Added web font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,13 @@ module.exports = {
         path: `${__dirname}/src/img/`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Barlow Condensed']
+        }
+      }
+    }
   ],
 }

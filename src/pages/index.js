e4294c3d0a4373e9b6bbaaa307b701fab73815fd
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-05-31 17:07:07 -0700

    Formatted page and added responsiveness to media titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Layout from '../components/Layout'
-import Head from '../components/Head'
-import Video from '../components/Video'
-import {graphql} from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Layout from "../components/Layout"
+import Head from "../components/Head"
+import Video from "../components/Video"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "../../scss/index.scss"
 
 export const query = graphql`
   query {
@@ -24,63 +25,70 @@ export const query = graphql`
     }
   }
 `
-const Home = ({data}) => {
+const Home = ({ data }) => {
   const files = data.allFile.edges
-  const homeImages = files.filter(file => file.node.name.includes('home'))
+  const homeImages = files.filter(file => file.node.name.includes("home"))
   const imageColumns = homeImages.map(img => {
     return (
-      <Col key={img.node.name} className="my-auto"><Img fluid={img.node.childImageSharp.fluid}/></Col>
+      <Col key={img.node.name} className="my-auto">
+        <Img fluid={img.node.childImageSharp.fluid} />
+      </Col>
     )
   })
 
   return (
-  <Layout>
-    <>
-      <Head title="Home" />
-      <Container>
-        <Row className="my-5">
-          {imageColumns}
-        </Row>
+    <Layout>
+      <>
+        <Head title="Home" />
+        <Container>
+          <Row className="my-5">{imageColumns}</Row>
 
-        <Row className="mb-5">
-          <Col>
-            <h2>Check out the most recent episode of Café con Leslie</h2>
-            <Video
-              videoSrcURL="https://www.youtube.com/embed/?listType=playlist&list=PLI06p-PFP4Line6k_PObdjbCN7zb11mok&index=7"
-              videoTitle="Latest Video"
-            />
-          </Col>
-        </Row>
-
-        <Row className="mb-5">
-          <Col>
-            <h2>Check out my guest appearance on Wassup Flobo? After Hours!</h2>
-            <Video
-              videoSrcURL="https://www.youtube.com/embed/VlvTNjfqjZ4"
-              videoTitle="Latest Video"
-            />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <h2>Check out my guest appearance on the Knew Amsterdam Radio</h2>
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                title="Knew Amsterdam Podcast"
-                src="https://open.spotify.com/embed-podcast/episode/0o2rwDzf8WM4NnJ57jQRMY"
-                width="100%"
-                height="380"
-                frameBorder="0"
-                allowtransparency="true"
-                allow="encrypted-media"
+          <Row className="mb-5">
+            <Col>
+              <h2 className="responsive">
+                Check out the most recent episode of Café con Leslie
+              </h2>
+              <Video
+                videoSrcURL="https://www.youtube.com/embed/?listType=playlist&list=PLI06p-PFP4Line6k_PObdjbCN7zb11mok&index=7"
+                videoTitle="Latest Video"
               />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  </Layout>
-)}
+            </Col>
+          </Row>
+
+          <Row className="mb-5">
+            <Col>
+              <h2 className="responsive">
+                Check out my guest appearance on Wassup Flobo? After Hours!
+              </h2>
+              <Video
+                videoSrcURL="https://www.youtube.com/embed/VlvTNjfqjZ4"
+                videoTitle="Latest Video"
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <h2 className="responsive">
+                Check out my guest appearance on the Knew Amsterdam Radio
+              </h2>
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                  title="Knew Amsterdam Podcast"
+                  src="https://open.spotify.com/embed-podcast/episode/0o2rwDzf8WM4NnJ57jQRMY"
+                  width="100%"
+                  height="380"
+                  frameBorder="0"
+                  allowtransparency="true"
+                  allow="encrypted-media"
+                />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    </Layout>
+  )
+}
 
 export default Home

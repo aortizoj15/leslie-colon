0d73dd2abe5ad063ed2c9b59ff6f00a2b4d449ec
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-05-31 17:02:39 -0700

    Removed class that made footer sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import '../../scss/footer.scss'
 
 const Footer = () => (
-    <footer className="text-muted footer fixed-bottom text-center p-1">
-      Made with Gatsby by <a className="footer-link" href="https://www.linkedin.com/in/alexis-ortiz-ojeda/">Alexis Ortiz Ojeda</a>
-    </footer>
+  <footer className="text-muted footer w-100 text-center p-1">
+    Made with Gatsby by <a className="footer-link" href="https://www.linkedin.com/in/alexis-ortiz-ojeda/">Alexis Ortiz Ojeda</a>
+  </footer>
 )
 
 export default Footer
